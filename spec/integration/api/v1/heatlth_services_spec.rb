3d086e6ffbffddb280e0ee62fbb28e667a9d2d43
juharyimami <juharyimami2011@gmail.com>
2023-06-21 12:29:49 +0300

    linter errors fixed

--- spec/integration/api/v1/heatlth_services_spec.rb
+++ spec/integration/api/v1/heatlth_services_spec.rb
@@ -1,12 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/heatlth_services', type: :request do
-
   path '/api/v1/heatlth_services' do
-
     get('list heatlth_services') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -20,7 +17,6 @@ RSpec.describe 'api/v1/heatlth_services', type: :request do
 
     post('create heatlth_service') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
